895c2f579705a76006f15c99844c47207bfa5c1d
jw20191n <clairejw1015@gmail.com>
2019-10-04 23:03:44 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let obj = array.find(function(obj){ return obj["result"] === "W"})
+  if (obj === undefined){
+    return undefined;
+  }else{
+    return obj["year"];
+  }
+}
+ 
+
